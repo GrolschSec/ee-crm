2255550896575b6b898bea7ae7a99292b24b1730
rlouvrie <rlouvrie@student.42.fr>
2024-03-08 07:49:07 +0100

    updating config

--- app/config/database.py
+++ app/config/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from settings import DATABASE
+from app.config.settings import DATABASE
 
 DATABASE_URL = f"{DATABASE['ENGINE']}://{DATABASE['USER']}:{DATABASE['PASSWORD']}@{DATABASE['HOST']}:{DATABASE['PORT']}/{DATABASE['NAME']}"
 
--- app/config/settings.py
+++ app/config/settings.py
@@ -16,3 +16,9 @@ DATABASE = {
     "USER": getenv("DB_USER"),
     "PASSWORD": getenv("DB_PASSWORD"),
 }
+
+JWT = {
+	"SECRET": getenv("JWT_SECRET"),
+	"ALGORITHM": getenv("JWT_ALGORITHM"),
+	"TOKEN_LIFETIME": int(getenv("JWT_TOKEN_LIFETIME")),
+}
