8d186e5c904dc89478c8e37b25aae21338966d7d
rlouvrie <rlouvrie@student.42.fr>
2024-04-04 18:17:09 +0200

    adding help message to the commands

--- app/views/main.py
+++ app/views/main.py
@@ -7,18 +7,18 @@ from app.views.client import ClientView
 app = Typer()
 
 
-@app.command()
+@app.command(help="Login to the system.")
 def login(email: str, password: str = None):
     view = LoginView()
     view.dispatch(email=email, password=password)
 
 
-@app.command()
+@app.command(help="Logout from the system.")
 def logout():
     view = LogoutView()
     view.dispatch()
 
 
-app.add_typer(UserView().app, name="user")
+app.add_typer(UserView().app, name="user", help="User management commands.")
 
-app.add_typer(ClientView().app, name="client")
+app.add_typer(ClientView().app, name="client", help="Client management commands.")
