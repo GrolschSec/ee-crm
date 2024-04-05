96c8348359b4a9aa520651e94f1527850b1e0531
rlouvrie <rlouvrie@student.42.fr>
2024-04-05 10:33:08 +0200

    removing unused comments

--- app/controllers/client.py
+++ app/controllers/client.py
@@ -14,23 +14,3 @@ class ClientController(ModelController):
             self.errors["fullname"] = "Fullname must contain only alphabets."
             return False
         return True
-
-    # is there a way to check if a client with the given field and value exists?
-    # by trying to create a client with the same field and value and catching the exception if it exists
-    # i should be able to do that since for the email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-    # phone: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
-
-
-#     @classmethod
-#     def validate_phone(cls, phone):
-#         message = "invalid phone number."
-#         try:
-#             parsed_phone = parse(phone, PHONE_REGION)
-#             normalized_phone = format_number(parsed_phone, PhoneNumberFormat.E164)
-#             if not is_valid_number(parsed_phone):
-#                 return [False, message]
-#         except phonenumberutil.NumberParseException:
-#             return [False, message]
-#         if cls.phone_exist(normalized_phone):
-#             return [False, "a client with this phone number already exists."]
-#         return [True, normalized_phone]
